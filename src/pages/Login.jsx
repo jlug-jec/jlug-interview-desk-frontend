60c9477bc4fc979829b031d6cb2daea2e28d26ac
EnigmaByte7 <saxenay117@gmail.com>
2024-12-14 14:59:35 +0530

    rbac

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -34,7 +34,7 @@ const Login = () => {
                 localStorage.setItem('user', JSON.stringify(res.user));
                 localStorage.setItem('userid', JSON.stringify(res.userID));
                 alert(res.message);
-                if (res.user.email.includes('admin')) {
+                if (res.user.email.includes('jlug')) {
                     navigate('/admin');
                 } else {
                     navigate('/userd');
